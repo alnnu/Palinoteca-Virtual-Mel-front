3b01291d73e8954406218ecea034e050352ca17a
alnnu <luannf1020@gmail.com>
2025-04-19 11:39:06 -0300

    add: load spin component

--- src/components/analises/loadSpin.tsx
+++ src/components/analises/loadSpin.tsx
@@ -2,8 +2,6 @@ import {Flex, Spin} from "antd";
 
 export default function LoadSpin() {
     return (
-
-
             <Flex vertical={true} justify={"center"} align={"center"} gap={35} className="w-full ">
                 <div>Sua imagem esta sendo analisada</div>
                 <Spin size="large" />
