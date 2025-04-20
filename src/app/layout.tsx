cd7c24288dab00ad78601a1048cd3de528543728
pedrofff <pmendes09@hotmail.com>
2025-04-20 00:48:04 -0300

    🐛 Fix antdesing warning error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/providers/SessionProvider";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
+import { ConfigProvider } from "antd";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,9 +31,11 @@ export default async function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
-          <AntdRegistry>
-            {children}
-          </AntdRegistry>
+          <ConfigProvider wave={{ disabled: true }}>
+            <AntdRegistry>
+              {children}
+            </AntdRegistry>
+          </ConfigProvider>
         </AuthProvider>
       </body>
     </html>
